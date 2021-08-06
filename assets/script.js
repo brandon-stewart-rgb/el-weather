1416d52f6509a7e73fd9637fb7acc06c84cef621
brandon-stewart-rgb <brandon@pixeltree.us>
2021-08-06 11:13:01 -0600

    HTTPS fetch update

--- assets/script.js
+++ assets/script.js
@@ -54,8 +54,8 @@ button.addEventListener('click',function(e){
     });
 
         
-    // get latitude and longitude from current input value...unable to publish as it does not use HTTPS
-   fetch('http://api.positionstack.com/v1/forward?access_key=bd7525d294dee49bf6525fc33d3c9013&query='+inputValue.value+'')
+    // get latitude and longitude from current input value...
+   fetch('https://api.positionstack.com/v1/forward?access_key=8b6705bd3db1ed3c15005b1f93926e8e&query='+inputValue.value+'')
    
    .then(responses => responses.json())
    .then(data=> {
